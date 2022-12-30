d8046eb6283b60f8c072f8d60b6eaa014a716156
NileshPatel83 <hselin_83@yahoo.com>
2022-12-30 13:42:34 +1000

    getSelectedCharacterTypes added to get user seletion for character type.

--- script.js
+++ script.js
@@ -1,36 +1,75 @@
-// Assignment Code
+//Global variables.
+const minLength = 8;
+const maxLength = 128;
+
+//Selects generate password button.
 var generateBtn = document.querySelector("#generate");
+
+//Event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 function writePassword() {
   var password = generatePassword();
-  if(password == null){
-    return null;
-  }
+  if(password == null) return;
 
   //Selects password display textbox.
   var passwordText = document.querySelector("#password");
 
   //Sets the text of password display textbox to generated password.
   passwordText.value = password;
-
 }
 
 function generatePassword(){
 
+  //Gets user entered password length.
   var passwordLength = getPasswordLength();
-  if(passwordLength == null){
-    return null;
-  }
+  if(passwordLength == null) return null;
+
+  //Gets user selected character types for password.
+  var charTypes = getSelectedCharacterTypes();
+  if(charTypes == null) return null;
+
+
+
+
+
+
+
+
   
-  console.log(passwordLength);
 }
 
-//Function to get the user input password lenght.
+//Gets user selected character types for password.
+//Displays an error message if none of lowercase, uppercase, numeric or special characters is selected.
+function getSelectedCharacterTypes(){
+  
+  //Initialize the object with false values.
+  var charTypes = {
+    includeLowercase : false,
+    includeUppercase : false,
+    includeNumbers : false,
+    includeSpecialChars : false,
+  };
+
+  //Prompts the user to select character types for password.
+  charTypes.includeLowercase = window.confirm("Would you like to include lower case letters?");
+  charTypes.includeUppercase = window.confirm("Would you like to include upper case letters?");
+  charTypes.includeNumbers = window.confirm("Would you like to include numbers?");
+  charTypes.includeSpecialChars = window.confirm("Would you like to include special characters?");  
+
+  //Checks if user has selected at least one character type or not. Displays an error message otherwise.
+  if(!charTypes.includeLowercase && !charTypes.includeUppercase && !charTypes.includeNumbers && !charTypes.includeSpecialChars){
+    window.alert("No character type selected. \nAt least one character type must be selected from lowercase, uppercase, numeric and special characters.\nPlease try again.");
+    return null;
+  }
+
+  return charTypes;
+}
+
+//Gets the user entered password length.
 //Display an error message, if entered value is not a number or less than 8 or greater than 128.
 function getPasswordLength(){
-  const minLength = 8;
-  const maxLength = 128;
-  
+
   //Promts the user to enter the value as password length.
   var passwordLength = window.prompt("Please enter the length of the password.\nPassword length must be between " + minLength + " and " + maxLength + " characters.");
 
@@ -42,6 +81,3 @@ function getPasswordLength(){
 
   return passwordLength;
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
